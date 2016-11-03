71f6248d7c0ca54c8ceb7e45b664d6e17bea32ea
Chris Kuske <chriskuske@gmail.com>
2016-11-03 06:38:18 -0700

    Added support to track inconsistencies, and new constructor

--- consistency.py
+++ consistency.py
@@ -1,6 +1,6 @@
 # 3 Oct 2016
-#  start with a known consistent matrix and then measure the number of inconsistnc detected. (and how does it scale with the size of the matrix)
-# keep track of the locations of the inconsistent items
+#  start with a known consistent matrix and then measure the number of inconsistnc detected. (and how does it scale with the size of the matrix) - done
+# keep track of the locations of the inconsistent items - done
 
 # 10 Oct 2016
 # besides the direct check of 'b', how many other cycles is 'b' involved in (where it is the Aik or Ajk)
@@ -22,9 +22,11 @@ root = 0
 class PairwiseMatrix:
     matrixSize = 0  # NxN matrix
 
-    def __init__(self):
+    def __init__(self, data=[]):
         self.matrixData = []
         self.matrixSize = 0
+        self.inconsistentLocations = []
+        self.inconsistenciesDetected = 0
 
     def setWindowRoot(self, root):
         self.root = root
@@ -36,13 +38,15 @@ class PairwiseMatrix:
     def PrintMatrix(self):
         for i in range(0,self.matrixSize):
             print self.matrixData[i]
-        print ""
 
     def GetSize(self):
         return self.matrixSize
 
     def GetRow(self,i):
         return self.matrixData[i]
+
+    def PrintInconsistencies(self):
+        print self.inconsistentLocations
 
     def GetItem(self,x,y):
         if x > self.matrixSize - 1:
@@ -59,20 +63,22 @@ class PairwiseMatrix:
 
     def CheckMatrixConsistency(self):
 
+        self.inconsistenciesDetected = 0
         n = self.matrixSize
 
         for i in range(0, n):
             for j in range(0, n):
                 for k in range(0, n):
 
-                    # if i == j:
+                    if i == j:
                     #    matrixSquares[i][j].config(text="1")
-                        # continue
+                    continue
 
+                    # Dij = Dik * Dkj
                     # print "i=" + str(i) + ", j=" + str(j) + ", k=" + str(k) + " -> " + \
-                    #       str(self.GetItem(i, k)) + "[" + str(i) + "," + str(k) + "] * " + str(
-                    #     self.GetItem(k, j)) + "[" + str(
-                    #     k) + "," + str(j) + "] should be " + str(self.GetItem(i, j))
+                    #      str(self.GetItem(i, k)) + "[" + str(i) + "," + str(k) + "] * " + str(
+                    #      self.GetItem(k, j)) + "[" + str(
+                    #      k) + "," + str(j) + "] should be " + str(self.GetItem(i, j))
 
                     (ijNum, ijDenom) = GetNumeratorDenominator(self.GetItem(i, j))
                     (ikNum, ikDenom) = GetNumeratorDenominator(self.GetItem(i, k))
@@ -86,17 +92,15 @@ class PairwiseMatrix:
 
                     (a, b) = simplify_fraction(ijTop, ijBottom)
 
-                    if (int(a) != int(ijNum)) and (int(b) != int(ijDenom)):
-                        print "A=" + str(a)
-                        print "B=" + str(b)
-                        print "ijNum=" + str(ijNum)
-                        print "ijDenom=" + str(ijDenom)
-                        return False
+                    if (int(a) != int(ijNum)) or (int(b) != int(ijDenom)):
+                        # print "A=" + str(a)
+                        # print "B=" + str(b)
+                        # print "ijNum=" + str(ijNum)
+                        # print "ijDenom=" + str(ijDenom)
+                        self.inconsistentLocations.append([i, j, k])
+                        self.inconsistenciesDetected += 1
 
-                        # print ""
-
-
-        return True
+        return (self.inconsistenciesDetected, self.inconsistentLocations)
 
     def GetDistance(self, oMatrix):
         totalDistance = 0.0
@@ -222,65 +226,6 @@ def GetNumeratorDenominator(fractionStr):
     else:
         return items[0], items[1]
 
-    def CheckMatrixConsistency(self):
-        n = self.matrixSize
-        for i in range(0, n):
-            for j in range(0, n):
-                for k in range(0, n):
-
-                    if i == j:
-                        matrixSquares[i][j].config(text="1")
-                        # continue
-
-                    print "i=" + str(i) + ", j=" + str(j) + ", k=" + str(k) + " -> " + \
-                          str(self.GetItem(i, k)) + "[" + str(i) + "," + str(k) + "] * " + str(
-                        self.GetItem(k, j)) + "[" + str(
-                        k) + "," + str(j) + "] should be " + str(self.GetItem(i, j))
-
-                    (ijNum, ijDenom) = GetNumeratorDenominator(self.GetItem(i, j))
-                    (ikNum, ikDenom) = GetNumeratorDenominator(self.GetItem(i, k))
-                    (kjNum, kjDenom) = GetNumeratorDenominator(self.GetItem(k, j))
-                    print(ijNum, ijDenom)
-                    print(ikNum, ikDenom)
-                    print(kjNum, kjDenom)
-
-                    ijTop = (int(ikNum) * int(kjNum))
-                    ijBottom = int(ikDenom) * int(kjDenom)
-
-                    (a, b) = simplify_fraction(ijTop, ijBottom)
-
-                    if (int(a) != int(ijNum)) and (int(b) != int(ijDenom)):
-                        print "A=" + str(a)
-                        print "B=" + str(b)
-                        print "ijNum=" + str(ijNum)
-                        print "ijDenom=" + str(ijDenom)
-                        return False
-
-                    matrixSquares[i][j].config(bg="#ff0000")  # red
-                    matrixSquares[i][k].config(bg="#ffff00")  # yellow
-                    matrixSquares[k][j].config(bg="#00ff00")  # green
-                    root.update()
-                    root.update_idletasks()
-                    # time.sleep(1)
-                    #
-
-                    self.root.update()
-                    self.root.update_idletasks()
-
-                    matrixSquares[i][j].config(bg="#ffffff")
-                    matrixSquares[i][k].config(bg="#ffffff")
-                    matrixSquares[k][j].config(bg="#ffffff")
-                    root.update()
-                    root.update_idletasks()
-                    print ""
-
-        return True
-
-
-
-
-# check that Dij = Dik * Dkj
-
 def callback():
     global matrixInputSquares
     # print matrixSquares
@@ -319,18 +264,63 @@ def center(win):
 
 
 if __name__ == "__main__":
-    allOnes = PairwiseMatrix()
-    allOnes.AddMatrixRow(['1', '1', '1'])
-    allOnes.AddMatrixRow(['1', '1', '1'])
-    allOnes.AddMatrixRow(['1', '1', '1'])
-    allOnes.PrintMatrix()
 
-    #1 3 5;2/6 1 10/6;1/5 3/5 1
-    p = PairwiseMatrix()
-    p.AddMatrixRow(['1','3', '5'])
-    p.AddMatrixRow(['1/3', '1', '5/3'])
-    p.AddMatrixRow(['1/5','3/5','1'])
-    p.PrintMatrix()
+    oneTwo = PairwiseMatrix()
+    oneTwo.AddMatrixRow(['1', '1', '2'])
+    oneTwo.AddMatrixRow(['1', '1', '1'])
+    oneTwo.AddMatrixRow(['1', '1', '1'])
+    oneTwo.PrintMatrix()
+    (noi, consistent) = oneTwo.CheckMatrixConsistency();
+    print "Matrix Diagonal Is 'Good': " + str(oneTwo.CheckMatrixDiagonal())
+    print "Matrix Is Consistent: " + str(noi == 0)
+    if noi > 0:
+        print "Number on Inconsistencies: " + str(noi)
+        print "Inconsistent Locations: "
+        oneTwo.PrintInconsistencies()
+
+    print ""
+    fixedOneTwo = PairwiseMatrix()
+    fixedOneTwo.AddMatrixRow(['1', '1', '2'])
+    fixedOneTwo.AddMatrixRow(['1', '1', '2'])
+    fixedOneTwo.AddMatrixRow(['1/2', '1/2', '1'])
+    fixedOneTwo.PrintMatrix()
+    (noi, consistent) = fixedOneTwo.CheckMatrixConsistency();
+    print "Matrix Diagonal Is 'Good': " + str(fixedOneTwo.CheckMatrixDiagonal())
+    print "Matrix Is Consistent: " + str(noi == 0)
+    if noi > 0:
+        print "Number on Inconsistencies: " + str(noi)
+        fixedOneTwo.PrintInconsistencies()
+
+    (distanceFromOneTwo, numberOfDiffs) = oneTwo.GetDistance(fixedOneTwo)
+    print ""
+    print "Distance from allOnes to fixedOneTwo: " + str(distanceFromOneTwo)
+    print "Number of differences: " + str(numberOfDiffs)
+
+    # allOnes = PairwiseMatrix()
+    # allOnes.AddMatrixRow(['1', '1', '1'])
+    # allOnes.AddMatrixRow(['1', '1', '1'])
+    # allOnes.AddMatrixRow(['1', '1', '1'])
+    # allOnes.PrintMatrix()
+    # print "Matrix Diagonal Is 'Good': " + str(allOnes.CheckMatrixDiagonal())
+    #
+    # print "Matrix Is Consistent: " + str(allOnes.CheckMatrixConsistency()[0] == 0)
+    # if allOnes.CheckMatrixConsistency()[0] != 0:
+    #     allOnes.PrintInconsistencies()
+    #
+    # distanceTup = allOnes.GetDistance(allOnes)
+    # print "Total Distance between matrices = " + str(distanceTup[0]) + ", differences = " + str(distanceTup[1])
+    # print ""
+    #
+    # #1 3 5;2/6 1 10/6;1/5 3/5 1
+    # p = PairwiseMatrix()
+    # p.AddMatrixRow(['1','3', '5'])
+    # p.AddMatrixRow(['1/3', '1', '5/3'])
+    # p.AddMatrixRow(['1/5','3/5','1'])
+    # p.PrintMatrix()
+    # print "Matrix Diagonal Is 'Good': " + str(p.CheckMatrixDiagonal())
+    # print "Matrix Is Consistent: " + str(p.CheckMatrixConsistency()[0] == 0)
+    # distanceTup = p.GetDistance(allOnes)
+    # print "Total Distance between matrices = " + str(distanceTup[0]) + ", differences = " + str(distanceTup[1])
 
     # p2 = PairwiseMatrix()
     # p2.AddMatrixRow(['2', '3', '5'])
@@ -338,8 +328,4 @@ if __name__ == "__main__":
     # p2.AddMatrixRow(['1/5', '7/10', '1'])
     # p2.PrintMatrix()
 
-    print "Matrix Diagonal Is 'Good': " + str(p.CheckMatrixDiagonal())
-    print "Matrix Is Consistent: " + str(p.CheckMatrixConsistency())
 
-    distanceTup = p.GetDistance(allOnes)
-    print "Total Distance between matrices = " + str(distanceTup[0]) + ", differences = " + str(distanceTup[1])
